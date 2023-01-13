ba20275896e90f6d1caacdd5ea443c36aaadfbd1
Jason Kirchman <kirchman.jason@icloud.com>
2023-01-12 20:26:59 -0600

    feat: Link player data in DOM to player objects

---
+++ src/main.js
@@ -0,0 +1,21 @@
+var player1 = new Player("Jason", false);
+var player2 = new Player("Robro", true);
+var game = null;
+
+var player1Section = document.getElementById('player-one-section');
+var player1Name = document.getElementById('player-one-name');
+var player1Score = document.getElementById('player-one-score');
+var player2Section = document.getElementById('player-two-section');
+var player2Name = document.getElementById('player-two-name');
+var player2Score = document.getElementById('player-two-score');
+
+window.addEventListener('load', function() {
+    displayPlayerData();
+});
+
+function displayPlayerData() {
+    player1Name.innerText = player1.name;
+    player1Score.innerText = player1.wins;
+    player2Name.innerText = player2.name;
+    player2Score.innerText = player2.wins;
+}
