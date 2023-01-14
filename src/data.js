7686c1428c2e6715453dcaa97ac3058a4554798f
Jason Kirchman <kirchman.jason@icloud.com>
2023-01-13 21:37:41 -0600

    feat: Add ability to select a game and display the fighters

--- src/data.js
+++ src/data.js
@@ -8,13 +8,13 @@ var fightersData = {
         },
         {
             id: "Paper",
-            img: "assets/fighters/classic/rock.svg",
+            img: "assets/fighters/classic/paper.svg",
             color: '#60B2E5',
             beats:["Rock"]
         },
         {
             id: "Scissors",
-            img: "assets/fighters/classic/rock.svg",
+            img: "assets/fighters/classic/scissors.svg",
             color: '#FF8966',
             beats:["Paper"]
         }
@@ -22,31 +22,31 @@ var fightersData = {
     advanced: [
         {
             id: "Water",
-            img: "assets/fighters/classic/rock.svg",
+            img: "assets/fighters/advanced/water.svg",
             color: '#706C61',
             beats:["Fire", "Plant"]
         },
         {
             id: "Fire",
-            img: "assets/fighters/classic/rock.svg",
+            img: "assets/fighters/advanced/fire.svg",
             color: '#706C61',
             beats:["Plant", "Psychic"]
         },
         {
             id: "Plant",
-            img: "assets/fighters/classic/rock.svg",
+            img: "assets/fighters/advanced/plant.svg",
             color: '#706C61',
             beats:["Psychic", "Electric"]
         },
         {
             id: "Psychic",
-            img: "assets/fighters/classic/rock.svg",
+            img: "assets/fighters/advanced/psychic.svg",
             color: '#706C61',
             beats:["Electric", "Water"]
         },
         {
             id: "Electric",
-            img: "assets/fighters/classic/rock.svg",
+            img: "assets/fighters/advanced/electric.svg",
             color: '#706C61',
             beats:["Water", "Fire"]
         }
--- src/main.js
+++ src/main.js
@@ -8,11 +8,23 @@ var player1Score = document.getElementById('player-one-score');
 var player2Section = document.getElementById('player-two-section');
 var player2Name = document.getElementById('player-two-name');
 var player2Score = document.getElementById('player-two-score');
-
-
+var gameSelectorSection = document.getElementById('selection-section');
+var classicSelector = document.getElementById('classic-selection');
+var advancedSelector = document.getElementById('advanced-selection');
+var fightersSection = document.getElementById('fighters-selection')
 
 window.addEventListener('load', function() {
     displayPlayerData();
+});
+
+classicSelector.addEventListener('click', function() {
+    game = new Game(player1, player2, 'classic');
+    displayFightersSection();
+});
+
+advancedSelector.addEventListener('click', function() {
+    game = new Game(player1, player2, 'advanced');
+    displayFightersSection();
 });
 
 function displayPlayerData() {
@@ -20,4 +32,26 @@ function displayPlayerData() {
     player1Score.innerText = player1.wins;
     player2Name.innerText = player2.name;
     player2Score.innerText = player2.wins;
+}
+
+function displayFightersSection() {
+    hide(gameSelectorSection);
+    addFighters();
+    show(fightersSection);
+}
+
+function addFighters() {
+    var fighters = fightersData[game.gameMode];
+    fightersSection.innerHTML = '';
+    for(var i = 0; i < fighters.length; i++) {
+        fightersSection.innerHTML += `<img class="fighter" src="${fighters[i].img}" alt="${fighters[i].id} fighter" />`
+    }
+}
+
+function show(element) {
+    element.classList.remove('hidden');
+}
+
+function hide(element) {
+    element.classList.add('hidden');
 }
